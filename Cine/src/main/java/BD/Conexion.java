945293f73e5d6d552c3e58e2017ea1da887b5ecf
Jairo-Main <jairomain14@gmail.com>
2025-01-28 01:15:25 -0400

    Add files via upload
    Conexion con la BD

---
+++ Cine/src/main/java/BD/Conexion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BD;
+
+import com.mycompany.cine.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JAIRO MAIN
+ */
+public class Conexion {
+    
+    Connection conectar = null;
+    
+    String usuario = "postgres";
+    String contrasenia = "12841055";
+    String bd = "Sistema_Cine";
+    String ip = "localhost";
+    String puerto = "5432";
+    
+    String cadena = "jdbc:postgresql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion(){
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+            conectar = DriverManager.getConnection(cadena, usuario, contrasenia);
+            //JOptionPane.showMessageDialog(null, "Se conecto correctamente a la Base de Datos");
+           
+        } catch (Exception e) {
+            JOptionPane.showInternalMessageDialog(null,"Error al conectar a la base de datos, error: "+e.toString());
+        }
+        
+        return conectar;
+    }
+    
+    public static void main(String[] args) {
+        Conexion objetoConexion = new Conexion();
+        objetoConexion.establecerConexion();
+    }
+}
